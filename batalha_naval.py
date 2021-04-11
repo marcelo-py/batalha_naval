89c45e714adffdddd01db36ebe5cf363684f7548
marcelo-py <79275219+marcelo-py@users.noreply.github.com>
2021-04-11 18:20:35 -0300

    Update batalha_naval.py

--- batalha_naval.py
+++ batalha_naval.py
@@ -11,16 +11,16 @@ jogador1 = input('Nome do Jogador 1').upper()[0]
 jogador2 = input('Nome do Jogador 2').upper()[0]
 
 for m in matriz.items():
-    print(m[0], end=' ')
+    print('\033[7;1;34m{}'.format(m[0], end=' '),end='')
     for l in m[1]:
         print('{}'.format(l),end='')
-    print()
+    print('\033[m')
 cont = 0
 while cont < s:
-    joga = int(input('{} =>joga em qual linha?'.format(jogador1)))
+    joga = int(input('\033[1;32m{}\033[m =>joga em qual linha?'.format(jogador1)))
     while joga > len(matriz.keys()):
         print('Digite uma linha existente!')
-        joga = int(input('{} =>joga em qual linha?'.format(jogador1)))
+        joga = int(input('\033[1;32m{}\033[m =>joga em qual linha?'.format(jogador1)))
 
     jogai = input('Qual indice? ').upper().replace('X', '')
     if jogai.isnumeric() or jogai.isalnum():
@@ -32,19 +32,21 @@ while cont < s:
         if jogai.isnumeric() or jogai.isalnum():
             jogai = int(jogai)
 
+        #se ja jogaram, o codigo
+    if 'X' not in matriz[joga][jogai][0]:
+        print('Esse capo ja tem Soldado')
+        continue
     matriz[joga][jogai] = [jogador1.center(2)]
-    print(type(matriz[joga][jogai]))
     for m in matriz.items():
-        print(m[0], end=' ')
+        print('\033[7;1;34m{}'.format(m[0], end=' '), end='')
         for l in m[1]:
             print('{}'.format(l), end='')
-        print()
+        print('\033[m')
 
-
-    jogada2 = int(input('{} =>joga em qual linha?'.format(jogador2)))
+    jogada2 = int(input('\033[1;32m{}\033[m =>joga em qual linha?'.format(jogador2)))
     while jogada2 > len(matriz.keys()):
         print('Digite uma linha existente!')
-        jogada2 = int(input('{} =>joga em qual linha?'.format(jogador2)))
+        jogada2 = int(input('\033[1;32m{}\033[m =>joga em qual linha?'.format(jogador2)))
 
     jogai2 = input('Qual indice? ').upper().replace('X', '')
     if jogai2.isnumeric() or jogai2.isalnum():
@@ -56,21 +58,36 @@ while cont < s:
         if jogai2.isnumeric() or jogai2.isalnum():
             jogai2 = int(jogai2)
 
+
+    while 'X' not in matriz[jogada2][jogai2][0]:
+        print('Atenção! Campo com soldado')
+        jogada2 = int(input('{} =>joga em qual linha?'.format(jogador2)))
+        while jogada2 > len(matriz.keys()):
+            print('Digite uma linha existente!')
+            jogada2 = int(input('\033[1;32m{}\033[m =>joga em qual linha?'.format(jogador2)))
+
+        jogai2 = input('Qual indice? ').upper().replace('X', '')
+        if jogai2.isnumeric() or jogai2.isalnum():
+            jogai2 = int(jogai2)
+
+        while jogai2 > len(matriz[0]):
+            print('Esse idice não existe! ')
+            jogai2 = input('Qual indice? ').upper().replace('X', '')
+            if jogai2.isnumeric() or jogai2.isalnum():
+                jogai2 = int(jogai2)
+
     matriz[jogada2][jogai2] = [jogador2.center(2)]
 
     for m in matriz.items():
-        print(m[0], end=' ')
+        print('\033[7;1;34m{}'.format(m[0], end=' '), end='')
         for l in m[1]:
             print('{}'.format(l), end='')
-        print()
-    cont += 1
+        print('\033[m')
+    cont += 2
     print('-'*30)
-    print(matriz)
-
+print('\033[34mResultado final\033[m')
 for m in matriz.items():
-    print(m[0], end=' ')
+    print('\033[7;1;34m{}'.format(m[0], end=' '),end='')
     for l in m[1]:
         print('{}'.format(l),end='')
-    print()
-
-
+    print('\033[m')
